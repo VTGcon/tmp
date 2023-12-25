f29fc23e1cc7cf64e4a61809fb51b4efaad79c47
Vyacheslav Shabes <71638897+VTGcon@users.noreply.github.com>
2023-12-25 12:47:24 +0300

    add hw3

--- consumer_1.py
+++ consumer_1.py
@@ -3,7 +3,7 @@ from kafka import KafkaConsumer
 
 def create_consumer():
     print("Connecting to Kafka brokers")
-    consumer = KafkaConsumer("itmo2023processed",
+    consumer = KafkaConsumer("itmo2023_processed",
                              group_id="itmo_processed_group",
                              bootstrap_servers='localhost:29092',
                              auto_offset_reset='earliest',
